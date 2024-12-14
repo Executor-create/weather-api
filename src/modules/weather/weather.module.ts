d5d82fe8f8ea886eae1e45975739e6ca246ddc90
Executor-create <just4dotaalex@gmail.com>
2024-12-14 20:39:46 +0200

    feat: add weather entity

--- src/modules/weather/weather.module.ts
+++ src/modules/weather/weather.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { WeatherService } from './weather.service';
+import { WeatherController } from './weather.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Weather } from './entities/weather.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Weather])],
+  providers: [WeatherService],
+  controllers: [WeatherController],
+})
 export class WeatherModule {}
