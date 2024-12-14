93d9245e843d44affdd005914c2b5f46f7376b76
Executor-create <just4dotaalex@gmail.com>
2024-12-14 20:36:19 +0200

    feat: add endpoint to fetch and save weather data

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateWeatherDto } from './dto/create-weather.dto';
+import { WeatherService } from './weather.service';
 
 @Controller('weathers')
-export class WeatherController {}
+export class WeatherController {
+  constructor(private readonly weatherService: WeatherService) {}
+
+  @Post()
+  async fetchAndSaveWeatherData(
+    @Body() createWeatherDto: CreateWeatherDto,
+  ): Promise<Record<string, any>> {
+    const weatherData =
+      await this.weatherService.saveWeatherData(createWeatherDto);
+
+    return weatherData;
+  }
+}
