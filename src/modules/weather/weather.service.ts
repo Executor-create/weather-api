d0d622b5098724baed01000b994fe05ab0a7fce8
Executor-create <just4dotaalex@gmail.com>
2024-12-14 20:37:10 +0200

    feat: add service to fetch and save weather data

--- src/modules/weather/weather.service.ts
+++ src/modules/weather/weather.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateWeatherDto } from './dto/create-weather.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Weather } from './entities/weather.entity';
 
 @Injectable()
-export class WeatherService {}
+export class WeatherService {
+  constructor(
+    @InjectRepository(Weather) private weatherRepository: Repository<Weather>,
+  ) {}
+
+  async saveWeatherData(createWeatherDto: CreateWeatherDto): Promise<Weather> {
+    try {
+      const { lat, lon, part } = createWeatherDto;
+
+      const fetchedWeather = await this.fetchWeatherData(lat, lon, part);
+      fetchedWeather.createdAt = new Date();
+
+      const weather = this.weatherRepository.create(fetchedWeather);
+
+      return await this.weatherRepository.save(weather);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async fetchWeatherData(
+    lat: number,
+    lon: number,
+    part: string,
+  ): Promise<Weather> {
+    try {
+      const apiKey = process.env.WEATHER_API_KEY;
+
+      if (!apiKey) {
+        throw new HttpException(
+          'Api key is missing',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+
+      const response = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`,
+      );
+
+      const weatherData = await response.json();
+
+      return weatherData;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
