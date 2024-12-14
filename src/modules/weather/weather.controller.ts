81bbae82ea871b48907167dd3597d8131283a3af
Executor-create <just4dotaalex@gmail.com>
2024-12-14 20:40:11 +0200

    feat: add endpoint to get weather data

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateWeatherDto } from './dto/create-weather.dto';
 import { WeatherService } from './weather.service';
+import { FormatWeatherInterceptor } from 'src/common/interceptors/format-weather.interceptor';
 
 @Controller('weathers')
 export class WeatherController {
@@ -12,6 +20,22 @@ export class WeatherController {
   ): Promise<Record<string, any>> {
     const weatherData =
       await this.weatherService.saveWeatherData(createWeatherDto);
+
+    return weatherData;
+  }
+
+  @Get()
+  @UseInterceptors(FormatWeatherInterceptor)
+  async getWeatherData(
+    @Query('lat') lat: number,
+    @Query('lon') lon: number,
+    @Query('part') part?: string,
+  ): Promise<Record<string, any>> {
+    const weatherData = await this.weatherService.getWeatherData(
+      lat,
+      lon,
+      part,
+    );
 
     return weatherData;
   }
